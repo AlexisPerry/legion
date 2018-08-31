ef19fec0f45da9e4e952024b67a7661ff7b6482d
Alexis Perry <aperry@lanl.gov>
2018-08-31 14:42:56 -0600

    code cleanup of runtime/realm/wrapper.cc

--- runtime/realm/wrapper.cc
+++ runtime/realm/wrapper.cc
@@ -14,42 +14,95 @@ extern "C" {
     std::set<Realm::Event> events;
     std::set<Realm::Event> mem_events;
     unsigned cur_task;
-    //Realm::ProfilingRequestSet prs; //the default profiling request set
   } context;
 
   static context *_globalCTX;  //global variable
   
   context * getRealmCTX() {
-    //std::cout << "start of getRealmCTX" << std::endl;
     if ( _globalCTX) {
-      //std::cout << "_globalCTX exists : returning it" << std::endl;
       return _globalCTX;
     }
     else {
-      //std::cout << "_globalCTX does not exist: returning a null pointer" << std::endl;
       return NULL;
     }
   }
   
   void realmInitRuntime(int argc, char** argv) {
-    std::cout << "start of realmInitRuntime" << std::endl;
-
     _globalCTX = new context();
-    std::cout << "context created" << std::endl;
 
     _globalCTX->rt = Realm::Runtime();
-    std::cout << "runtime object created" << std::endl;
-    _globalCTX->rt.init(&argc, &argv);
-    std::cout << "Runtime initialized" << std::endl;
+    _globalCTX->rt.init(&argc, &argv); 
     
     _globalCTX->cur_task = Realm::Processor::TASK_ID_FIRST_AVAILABLE;
-    std::cout << "curtask set" << std::endl;
-    
-    std::cout << "set _globalCTX - All Done" << std::endl;
 
     return;
   }
 
+  void realmSpawn(Realm::Processor::TaskFuncPtr func, const void* args, size_t arglen, void* user_data, size_t user_data_len);
+  void realmSpawn(Realm::Processor::TaskFuncPtr func, 
+		  const void* args, 
+		  size_t arglen, 
+		  void* user_data, 
+		  size_t user_data_len){ 
+    /* take a function pointer to the task you want to run, 
+       creates a CodeDescriptor from it directly
+       needs pointer to user data and arguments (NULL for void?)
+       needs size_t for len (0 for void?)
+     */
+
+    context *ctx = getRealmCTX();
+
+    //update current taskID
+    ctx->cur_task++;
+    Realm::Processor::TaskFuncID taskID = ctx->cur_task;
+
+    //get a processor to run on
+    Realm::Machine::ProcessorQuery procquery(Realm::Machine::get_machine());
+    Realm::Processor p = procquery.local_address_space().random();
+    assert ( p != Realm::Processor::NO_PROC); //assert that the processor exists
+
+    //get a memory associated with that processor to copy to
+    Realm::Machine::MemoryQuery memquery(Realm::Machine::get_machine());
+    Realm::Memory m = memquery.local_address_space().best_affinity_to(p).random();
+    assert ( m != Realm::Memory::NO_MEMORY); //assert that the memory exists
+
+    // Create a CodeDescriptor from the TaskFuncPtr   
+    Realm::CodeDescriptor cd = Realm::CodeDescriptor(func);
+
+    const Realm::ProfilingRequestSet prs;  //We don't care what it is for now, the default is fine
+
+    //register the task with the runtime
+    Realm::Event e1 = p.register_task(taskID, cd, prs, user_data, user_data_len);
+    ctx->events.insert(e1); //might not actually need to keep track of this one
+
+    //spawn the task
+    Realm::Event e2 = p.spawn(taskID, args, arglen, e1, 0); //predicated on the completion of the task's registration
+    ctx->events.insert(e2);
+
+    return;
+  }
+  
+  void realmSync() {
+    context *ctx = getRealmCTX();
+
+    //create an event that does not trigger until all previous events have triggered
+    Realm::Event e;
+    e = e.merge_events(ctx->events); 
+
+    //can clear the events in the list now and insert only the sync event
+    ctx->events.clear();
+    ctx->events.insert(e);
+
+    // Do not return until sync is complete
+    while (! e.has_triggered()) {
+      continue;
+    }
+
+    return;
+  }
+}
+
+/// BEGIN EXTRANEOUS STUFF THAT I'M NOT READY TO THROW AWAY YET
 #if 0
   //realmCreateRegion
   void* realmCreateRegion_int(int* data) {
@@ -191,140 +244,3 @@ extern "C" {
       return task;
   }
 #endif
-
-  //NOTE: this is for integers for now
-  //void realmSpawn(void (*func)(void), 
-  //void realmSpawn(const char *lib_name, const char *symbol_name, 
-  void realmSpawn(Realm::Processor::TaskFuncPtr func, const void* args, size_t arglen, void* user_data, size_t user_data_len);
-  void realmSpawn(Realm::Processor::TaskFuncPtr func, 
-		  const void* args, 
-		  size_t arglen, 
-		  void* user_data, 
-		  size_t user_data_len){ 
-    //		  void* data_region) {           
-    /* take a function pointer to the task you want to run, 
-       creates a CodeDescriptor from it
-       needs pointer to user data and arguments (NULL for void?)
-       needs size_t for len (0 for void?)
-       data_region is actually a pointer to a RegionInstance
-     */
-    std::cout << "start of realmSpawn" << std::endl;
-    context *ctx = getRealmCTX();
-    std::cout << "successfully got the realm context" << std::endl;
-
-    //update current taskID
-    ctx->cur_task++;
-    std::cout << "updated cur_task" << std::endl;
-    Realm::Processor::TaskFuncID taskID = ctx->cur_task;
-    std::cout << "created the taskID" << std::endl;
-
-    //get a processor to run on
-    Realm::Machine::ProcessorQuery procquery(Realm::Machine::get_machine());
-    Realm::Processor p = procquery.local_address_space().random();
-    assert ( p != Realm::Processor::NO_PROC); //assert that the processor exists
-
-    //get a memory associated with that processor to copy to
-    Realm::Machine::MemoryQuery memquery(Realm::Machine::get_machine());
-    Realm::Memory m = memquery.local_address_space().best_affinity_to(p).random();
-    assert ( m != Realm::Memory::NO_MEMORY); //assert that the memory exists
-
-    //takes fxn ptr, turns it into a TypeConv::from_cpp_type<TaskFuncPtr>()
-    // the CodeDescriptor needs to be of that type
-
-    // orig:  
-    Realm::CodeDescriptor cd = Realm::CodeDescriptor(func);
-    //old: Realm::CodeDescriptor cd = Realm::CodeDescriptor(Realm::TypeConv::from_cpp_type<Realm::Processor::TaskFuncPtr>());
-    //canonicalTask(args, arglen, user_data, user_data_len, p);
-    //Realm::CodeDescriptor cd = Realm::CodeDescriptor(canonicalTask);
-    std::cout << "Created CodeDescriptor" << std::endl;
-    //Realm::FunctionPointerImplementation fpi = Realm::FunctionPointerImplementation(func);
-    //std::cout << "Created FunctionPointerImplementation" << std::endl;
-    //Realm::DSOReferenceImplementation fpi = Realm::DSOReferenceImplementation(std::string(lib_name), std::string(symbol_name));
-    //cd.add_implementation(&fpi);
-    //std::cout << "added the implementation to the CodeDescriptor" << std::endl;
-
-    const Realm::ProfilingRequestSet prs;  //We don't care what it is for now, the default is fine
-    std::cout << "Created a default ProfilingRequestSet" << std::endl;
-
-
-    //create a physical region for the copy
-    //predicate creation of this region on the creation and initialization of the old region
-    //    Realm::Event mem_event = mem_sync();
-    //Realm::RegionInstance R;
-
-    //constexpr auto user_data_type = std::type_index(DTYPE);
-    //constexpr auto user_data_type = (constexpr)DTYPE.name();
-    //Realm::InstanceLayout<user_data_len,typeid(user_data[0]).name()> il;
-    //Realm::InstanceLayout<1,user_data_type> il = Realm::InstanceLayoutOpaque(user_data_len,alignof(user_data)); //alignment is what?
-    //Realm::InstanceLayout<1,typeid(user_element).name()> il = Realm::InstanceLayoutOpaque(user_data_len,alignof(user_data)); //alignment is what?
-    //const Realm::InstanceLayoutGeneric * il = ((Realm::RegionInstance *)data_region)->get_layout(); //copy the layout of the source region
-
-    //NOTE: the following is not implemented in realm, but only exists in a header file function declaration
-    // If implemented, it would eliminate the need for ctx->mem_events.
-    //Realm::Event regEvt = Realm::RegionInstance::create_instance(R,m,(Realm::InstanceLayoutGeneric *)il,prs, ((Realm::RegionInstance *)data_region)->get_ready_event());
-
-    //Realm::Event regEvt = Realm::RegionInstance::create_instance(R,m,(Realm::InstanceLayoutGeneric *)il,prs, mem_event);
-    //ctx->mem_events.insert(regEvt);
-    
-    //copy the user data to the region
-    //while (!regEvt.has_triggered())
-    //continue;
-    //R.write_untyped(0, user_data, user_data_len);
-    //for(auto fieldPair : il->fields) {
-    // Realm::FieldID fid = fieldPair.first;
-    //Realm::Event copyEvt = realmCopy_int(*((Realm::RegionInstance *)data_region), R, fid, regEvt);
-    //ctx->mem_events.insert(copyEvt);
-    //}
-    //std::cout << "Finished copy" << std::endl;
-
-    //register the task with the runtime
-    Realm::Event e1 = p.register_task(taskID, cd, prs, user_data, user_data_len);
-    std::cout << "Registered Task" << std::endl;
-    ctx->events.insert(e1); //might not actually need to keep track of this one
-    std::cout << "Added the register_task event to the context's set of Events" << std::endl;
-
-    //spawn the task
-    Realm::Event e2 = p.spawn(taskID, args, arglen, e1, 0); 
-    std::cout << "Spawned the task" << std::endl;
-    ctx->events.insert(e2);
-    std::cout << "Added the spawn event to the context's set of Events" << std::endl;
-
-    //copy the data back over
-    //for(auto fieldPair : il->fields) {
-    //  Realm::FieldID fid = fieldPair.first;
-    //  Realm::Event copyBackEvt = realmCopy_int(R,*((Realm::RegionInstance *)data_region), fid, e2); //predicated on completion of spawned task
-    //  ctx->mem_events.insert(copyBackEvt);
-    //}
-
-    //free the newly created region after copy back finishes
-    //Realm::Event allDone = mem_sync();
-    //while (! allDone.has_triggered()) {
-    //std::cout << "realmSpawn allDone event has NOT TRIGGERED" << std::endl;
-    // continue;
-    //}
-    //realmDestroyRegion((void*) &R);
-
-    return;
-  }
-  
-  void realmSync() {
-    std::cout << "Start of realmSync" << std::endl;
-    context *ctx = getRealmCTX();
-    Realm::Event e;
-    e = e.merge_events(ctx->events);
-    std::cout << "merged events" << std::endl;
-
-    ctx->events.clear();
-    std::cout << "Cleared the context's set of events" << std::endl;
-    ctx->events.insert(e);
-    std::cout << "Added sync event to context's set of events" << std::endl;
-
-    while (! e.has_triggered()) {
-      std::cout << "Sync event NOT TRIGGERED" << std::endl;
-      continue;
-    }
-    std::cout << "Sync event TRIGGERED" << std::endl;
-
-    return;
-  }
-}
